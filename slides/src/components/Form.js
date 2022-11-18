d2aab880d9a33d1f86c6b7bb0da70186301620d7
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-11-17 19:09:18 -0500

    added env file for api key, added title api call in form component

--- slides/src/components/Form.js
+++ slides/src/components/Form.js
@@ -1,23 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios';
 
 const Form = () => {
+    const [loaded, setLoaded] = useState(false)
+    const [option, setOption] = useState('title')
+    const [userSearch, setUserSearch] = useState('')
+    const [books, setBooks] = useState({});
+    
+    const Lookup = (e) => {
 
-    const Lookup = () => {
-        console.log('p')
+        //split into array and loop thru to concat api call
+
+        e.preventDefault();
+        let searchTerms = userSearch.split(' ')
+        // console.log(searchTerms)
+        let x = encodeURIComponent(JSON.stringify(searchTerms))
+        console.log(x)
+        console.log('lookup function before axios')
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${x}:keyes&key=${process.env.REACT_APP_API_KEY}`)
+        .then((res)=> {
+            console.log('yay', res.data.items)
+            setBooks(res.data.items)
+            setLoaded(true)
+        })
+        .catch((err)=> {
+            console.log('boo',err)
+        })
+
     }
 
 
     return (
-        <div className='d-flex flex-column border border-dark'>
-            <h2>What do you want to Book Up?</h2>
-            <form onSubmit={Lookup()} className='d-flex border border-dark w-50 mx-auto justify-content-evenly'>
-                <select>
-                    <option>Search By Title</option>
-                    <option>Search By Author</option>
-                    <option>Search By Genre</option>
-                </select>
-                <button className='btn btn-warning'>Search</button>
-            </form>
+        <div className='d-flex flex-column border border-dark container'>
+            <div className='bar row'>
+
+                <h2 className='col-12 col-sm-12 col-md-12 col-lg-12'>What do you want to Book Up?</h2>
+                <form onSubmit={Lookup} className='d-flex justify-content-around mx-auto border border-dark col-12 col-sm-12 col-md-10 col-lg-8'>
+                    <select onChange={(e)=>setOption(e.target.value)}>
+                        <option value='title'>Search By Title</option>
+                        <option value='author'>Search By Author</option>
+                        <option value='genre'>Search By Genre</option>
+                    </select>
+                    <input 
+                        type='text' 
+                        value={userSearch}
+                        onChange={(e)=>{setUserSearch(e.target.value)}}
+                        />
+                    <button className='btn btn-warning'>Search</button>
+                    {/* <button className='btn btn-warning'>Recommend a book!</button> */}
+                </form>
+            </div>
+            {
+                loaded==true?
+                books.map((item, index) => {
+                    return <div className='list row'>
+                                <ul key={index} className='d-flex justify-content-around mx-auto border border-dark col-12 col-sm-12 col-md-10 col-lg-8'>
+                                    <li>Title: {item.volumeInfo.title}</li>
+                                    <li>Author: {item.volumeInfo.authors[0]}</li>
+                                </ul>
+                            </div>
+                }):null
+            }
+            <div className='list row'>
+
+                <ul className='d-flex justify-content-around mx-auto border border-dark col-12 col-sm-12 col-md-10 col-lg-8'>
+
+                    <li >Test ttle</li>
+                    <li>Test author</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- slides/src/components/Main.js
+++ slides/src/components/Main.js
@@ -6,18 +6,14 @@ const Main = () => {
     const [data, setData] = useState({})
     const [loaded, setLoaded] = useState(false)
 
-    // useEffect(()=> {
-    //     axios.get('https://www.googleapis.com/books/v1/volumes?q=great+intitle:gatsby&key=AIzaSyD3CGNRmMnLGSyyzufMmphwyfV3XuXKPK0')
-    //     .then((res)=> {
-    //         console.log(res.data.items[0].volumeInfo.title)
-    //         setData(res.data.items[0].volumeInfo.title)
-    //         setLoaded(true)
-    //     })
-    //     .catch((err)=> {
-    //         console.log('womp', err)
-    //     })
-    // }, [])
+    useEffect(()=> {
+        
+        
+    },[])
 
+
+
+    
     return (
         <div>
             {
